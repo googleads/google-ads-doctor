6bb9eb7ef634905b9cf19208fad9b4d39f070f84
Poki Chui <pchui@google.com>
2019-05-17 15:57:35 -0400

    Displaying error messages with a better convention

--- oauthdoctor/diag/config.go
+++ oauthdoctor/diag/config.go
@@ -227,7 +227,7 @@ func (c *ConfigFile) ReplaceConfig(key, value string) string {
 	// Create a temp file
 	tmpfile, err := ioutil.TempFile("", "googleadsapi_client_lib_config")
 	if err != nil {
-		log.Fatalf("ERROR: Problem creating temp file.\nSystem error: %s", err)
+		log.Fatalf("ERROR: Problem creating temp file: %s", err)
 	}
 	defer tmpfile.Close()
 
@@ -235,14 +235,14 @@ func (c *ConfigFile) ReplaceConfig(key, value string) string {
 	configFp := filepath.Join(c.Filepath, c.Filename)
 	f, err := os.Open(configFp)
 	if err != nil {
-		log.Fatalf("ERROR: Problem opening config file.\nSystem error: %s", err)
+		log.Fatalf("ERROR: Problem opening config file: %s", err)
 	}
 	defer f.Close()
 
 	// Replace with new config value and write to temp file
 	newConfigStr := c.ReplaceConfigFromReader(key, value, f)
 	if _, err := tmpfile.Write([]byte(newConfigStr)); err != nil {
-		log.Fatalf("ERROR: Cannot write to temp config file (%s).\nSystem error: %s",
+		log.Fatalf("ERROR: Cannot write to temp config file (%s): %s",
 			tmpfile.Name(), err)
 	}
 
@@ -253,12 +253,12 @@ func (c *ConfigFile) ReplaceConfig(key, value string) string {
 	backupFp := configFp + "_" + time.Now().Format("2006-01-02_15-04-05")
 	log.Printf("Backing up config file %s to %s...", configFp, backupFp)
 	if err = os.Rename(configFp, backupFp); err != nil {
-		log.Fatalf("ERROR: Cannot rename config file from (%s) to (%s).\nSystem error: %s",
+		log.Fatalf("ERROR: Cannot rename config file from (%s) to (%s): %s",
 			configFp, backupFp, err)
 	} else {
 		log.Printf("Creating a new config file %s...", configFp)
 		if err = os.Rename(tmpfile.Name(), configFp); err != nil {
-			log.Fatalf("ERROR: Cannot rename config file from (%s) to (%s).\nSystem error: %s",
+			log.Fatalf("ERROR: Cannot rename config file from (%s) to (%s): %s",
 				tmpfile.Name(), configFp, err)
 		}
 	}
