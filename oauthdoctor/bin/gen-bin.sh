1eec7d9a37c8f683a556ddb4bb0e13c5394f4a79
Poki Chui <pchui@google.com>
2019-05-20 15:38:16 -0400

    Added checks for binary generation script

--- oauthdoctor/bin/gen-bin.sh
+++ oauthdoctor/bin/gen-bin.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-root=<Enter your Google Ads Doctor root path here>
-bin=$root/bin
+# Before you run this script, please make sure your current directory is inside
+# Google Ads Doctor git repo locally.
+
+# Show the absolute path of the top-level directory
+root=$(git rev-parse --show-toplevel)
+src=$root/oauthdoctor
+bin=$src/bin
+pwd=$PWD
 
 # Remove previously generated binaries
 rm -f $root/windows
@@ -9,19 +15,23 @@ rm -f $root/osx
 rm -f $root/linux
 
 # Generate binaries from master branch
-pushd $root
-orig_branch=$(git rev-parse --abbrev-ref HEAD)
-git checkout master
+cd $src
+cur_branch=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$cur_branch" != "master" ]] ; then
+  git checkout master
+fi
 git pull origin master
 
 # Generate binaries
-CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o $bin/windows/oauthdoctor-386.exe $root/oauthdoctor.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o $bin/windows/oauthdoctor-amd64.exe $root/oauthdoctor.go
-CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o $bin/linux/oauthdoctor-386 $root/oauthdoctor.go
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $bin/linux/oauthdoctor-amd64 $root/oauthdoctor.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=386 go build -o $bin/osx/oauthdoctor-386 $root/oauthdoctor.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $bin/osx/oauthdoctor-amd64 $root/oauthdoctor.go
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o $bin/windows/oauthdoctor-386.exe $src/oauthdoctor.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o $bin/windows/oauthdoctor-amd64.exe $src/oauthdoctor.go
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o $bin/linux/oauthdoctor-386 $src/oauthdoctor.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $bin/linux/oauthdoctor-amd64 $src/oauthdoctor.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=386 go build -o $bin/osx/oauthdoctor-386 $src/oauthdoctor.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $bin/osx/oauthdoctor-amd64 $src/oauthdoctor.go
 
 chmod 754 $bin/*/*
-git checkout $orig_branch
-popd
+if [[ "$cur_branch" != "master" ]] ; then
+  git checkout $cur_branch
+fi
+cd $pwd
